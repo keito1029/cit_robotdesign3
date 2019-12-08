ba77bef213af3033172a43b553d037d0262b1415
keito1029 <39695953+keito1029@users.noreply.github.com>
2019-12-09 07:27:09 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -30,10 +30,12 @@ function connect() {
             messageType: "sensor_msgs/Imu"
         });
         closemsg = new ROSLIB.Topic({
+        ros: ros,
         name        : '/button/close',
         messageType : "std_msgs/Bool"
         });
         openmsg = new ROSLIB.Topic({
+        ros: ros,
         name        : '/button/open',
         messageType : "std_msgs/Bool"
         });
@@ -109,13 +111,10 @@ var data_send = function () {
     }
 }
 
-        function close(){
-        close_msg.publish()
-        };
-        function open(){
-        open_msg.publish()
-        };
-
+        close_msg.publish(data);
+        
+        open_msg.publish(data);
+        
 window.addEventListener("devicemotion", devicemotionHandler);
 function devicemotionHandler(event) {
 
